e9ec2e28926e44575ea2c0a203363151527e0810
qq <qianq9720@163.com>
2023-04-16 13:32:00 +0800

    chat window 基础布局

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="chatWindow">
+    <div v-for="(msg, item) in msgArr" :sender_type="msg.sender_type">
+      <div class="msgTimestamp">{{ msg.timestamp }} => </div>
+      <div class="msgContent">{{ msg.content }}</div>
+    </div>
     
   </div>
 </template>
@@ -7,6 +11,30 @@
 <script>
 export default {
   name: 'ChatWindow',
+  data(){
+    return {
+      msgArr: [
+        {
+          timestamp: 1681622395180,
+          content: "aaa",
+          content_type: 0,
+          sender_type: 0 
+        },
+        {
+          timestamp: 1681622395180,
+          content: "bbb",
+          content_type: 0,
+          sender_type: 1 
+        }
+      ],
+      send_msg: {
+        timestamp: 0,
+        content: "",
+        content_type: 0,
+        sender_type: 0
+      }
+    }
+  }
 }
 </script>
 
