9c061074f27e9d9ae0fe8ac960ef07db9fb4e414
qq <qianq9720@163.com>
2023-04-16 12:18:15 +0800

    sidebar init

---
+++ src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navBar">
+    left nav111
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftNavBar',
+}
+</script>
+
+<style scoped>
+.navBar{
+  color: red;
+  
+}
+</style>
+
+
