d81801dee1aaf7401f4d59dd967da4b04202d53f
pico <qianq9720@163.com>
2023-04-17 15:03:33 +0800

    record

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -114,9 +114,10 @@ export default {
     },
     playMsgRecord(content){
       this.recorderObj.play(content);
+      // this.recorderObj.downloadWAV('test.wav');
     },
     sendChatAudioMsg(){
-      // TODO 网络请求发送
+      // 网络请求发送
       if(this.recorderObj.size!=0){
         console.log(this.audioMsg)
         this.audioMsg.timestamp = new Date().getTime();
@@ -148,7 +149,12 @@ export default {
   },
   mounted(){
     navigator.mediaDevices.getUserMedia({ audio: true }); 
-    let recorder = new Recorder();
+    let recorder = new Recorder({
+      sampleBits: 16,         // 采样位数，支持 8 或 16，默认是16
+      sampleRate: 16000,      // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
+      numChannels: 1,         // 声道，支持 1 或 2， 默认是1
+      compiling: false,       // 是否边录边转换，默认是false
+    });
     this.recorderObj = recorder;
   },
   updated(){
