3ccbc43328016f9d0b70f768e2d1bb0e383f04dd
pico <qianq9720@163.com>
2023-04-23 14:39:03 +0800

    cors problem

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    // 跨域问题解决 需要重启服务
-    proxy: {
-      '/': {
-        target: 'http://localhost:8888',
-        changeOrigin: true
-      }
-    }
-  }
 })
